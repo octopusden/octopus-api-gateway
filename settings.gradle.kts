bdb2664d63dd2d78cbeb3b56c521f3a2f6b21dc9
Philip Gorbachev <pgorbachev@openwaygroup.com>
2023-02-08 14:11:16 +0200

    #1 added repositories for plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,8 @@ pluginManagement {
         id("org.octopusden.release-management") version (extra["release-management.version"] as String)
         id("org.springframework.boot") version (extra["spring-boot.version"] as String)
     }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
 }
