c6b117b91c286a57fafd3db2cfc71c6a0c01a1a9
Philip Gorbachev <phil.gorbachev@gmail.com>
2023-02-09 19:32:39 +0200

    Setup release workflow #1 (#6)
    * #1 fixed rm plugin
    
    * #1 added central repository
    
    * #1 added nexus-publish plugin
    
    * #1 added nexus-publish plugin
    
    * #1 reverted maven-publish
    
    * #1 --info + BUILD_VERSION
    
    * #1 fixed publication
    
    * #1 added debug and stacktrace
    
    * #1 updated gradle wrapper to solve signing problem
    
    * #1 added javadoc + sources
    
    * #1 suppressed enforcer
    
    * #1 added javadoc + sources.jar
    
    * #1 reverted signing
    
    * #1 fixed docker
    
    * #1 fixed build-version
    
    * #1 fixed build-version
    
    * #1 fixed build-version
    
    * #1 added permission
    
    * #1 added permission
    
    * #1 fixed release tag
    
    * #1 fixed release tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,41 @@
 plugins {
     kotlin("jvm")
-    id("org.octopusden.release-management")
+    id("org.octopusden.octopus-release-management")
     id("org.springframework.boot")
     id("com.bmuschko.docker-spring-boot-application") version "7.1.0"
-    id("maven-publish")
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin")
+    signing
 }
 
 group = "org.octopusden.cloud.api-gateway"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<GenerateModuleMetadata> {
+    // The value 'enforced-platform' is provided in the validation
+    // error message
+    suppressedValidationErrors.add("enforced-platform")
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("MAVEN_USERNAME"))
+            password.set(System.getenv("MAVEN_PASSWORD"))
+        }
+    }
+}
 
 publishing {
     repositories {
@@ -16,10 +45,41 @@ publishing {
     }
     publications {
         create<MavenPublication>("bootJar") {
+            from(components["java"])
             artifact(tasks.getByName("bootJar"))
+            pom {
+                name.set(project.name)
+                description.set("Octopus module: ${project.name}")
+                url.set("https://github.com/octopusden/octopus-api-gateway.git")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/kzaporozhtsev/octopus-employee-service.git")
+                    connection.set("scm:git://github.com/octopusden/octopus-employee-service.git")
+                }
+                developers {
+                    developer {
+                        id.set("octopus")
+                        name.set("octopus")
+                    }
+                }
+            }
         }
     }
 }
+
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["bootJar"])
+}
+
 
 springBoot {
     buildInfo()
