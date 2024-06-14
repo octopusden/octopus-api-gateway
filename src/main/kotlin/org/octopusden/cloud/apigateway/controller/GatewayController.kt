9af0bf81bf314396310b8cc5b4df17e6af73230c
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2024-06-14 15:05:51 +0300

    Use former logout.html as main page. (#16)

--- src/main/kotlin/org/octopusden/cloud/apigateway/controller/GatewayController.kt
+++ src/main/kotlin/org/octopusden/cloud/apigateway/controller/GatewayController.kt
@@ -1,9 +1,6 @@
 package org.octopusden.cloud.apigateway.controller
 
-import org.octopusden.cloud.apigateway.config.SecurityConfig
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -13,17 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class GatewayController {
-    @GetMapping(SecurityConfig.LOGOUT_CUSTOM_ENDPOINT)
-    fun logout(
+    @GetMapping
+    fun index(
         model: Model,
-        @RegisteredOAuth2AuthorizedClient authorizedClient: OAuth2AuthorizedClient,
         @AuthenticationPrincipal oauth2User: OAuth2User,
-        @RequestParam(SecurityConfig.REDIRECT_URL_PARAM_NAME, defaultValue = "/") redirectUrl: String,
+        @RequestParam("redirect_url", defaultValue = "/dms-ui/") redirectUrl: String,
     ): String {
         model.addAttribute("userName", oauth2User.name)
-        model.addAttribute("clientName", authorizedClient.clientRegistration.clientName)
-        model.addAttribute("userAttributes", oauth2User.attributes)
         model.addAttribute("redirectUrl", redirectUrl)
-        return "logout"
+        return "index"
     }
 }
