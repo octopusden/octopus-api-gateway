a966cc7836bf516dfbf861034f5b1aa529ae6372
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2024-06-14 13:26:29 +0300

    Use former logout.html as main page.

--- src/main/kotlin/org/octopusden/cloud/apigateway/config/SecurityConfig.kt
+++ src/main/kotlin/org/octopusden/cloud/apigateway/config/SecurityConfig.kt
@@ -6,20 +6,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers
-import org.springframework.web.server.ServerWebExchange
-import org.springframework.web.server.WebFilter
-import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
-import java.net.URLEncoder
 
 
 @Configuration
@@ -30,7 +23,7 @@ open class SecurityConfig(@Value("\${auth-server.logout-url}") private val logou
     open fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         http.authorizeExchange { exchanges: AuthorizeExchangeSpec ->
             exchanges.pathMatchers("/dms-ui/actuator/**").permitAll()
-            exchanges.pathMatchers("/logout**", "/dms-ui/**").authenticated()
+            exchanges.pathMatchers("/","/dms-ui/**").authenticated()
             exchanges.anyExchange().permitAll()
         }
             .oauth2Login(Customizer.withDefaults())
@@ -40,33 +33,7 @@ open class SecurityConfig(@Value("\${auth-server.logout-url}") private val logou
                 exchange.exchange.response.headers.add(HttpHeaders.LOCATION, logoutUrl)
                 Mono.empty()
             }
-            .and()
-            .addFilterBefore(LogoutFilter(), SecurityWebFiltersOrder.LOGOUT_PAGE_GENERATING)
-            .csrf().disable()
-
+            .and().csrf().disable()
         return http.build()
-    }
-
-    private class LogoutFilter : WebFilter {
-        private val matcher = ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/logout")
-
-        override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> = matcher.matches(exchange)
-            .filter { matchResult -> matchResult.isMatch }
-            .switchIfEmpty(chain.filter(exchange).then(Mono.empty()))
-            .flatMap { _ ->
-                val response = exchange.response
-                response.statusCode = HttpStatus.FOUND
-                val redirectUrl = exchange.request
-                    .queryParams[REDIRECT_URL_PARAM_NAME]
-                    ?.joinToString(",")
-                    ?.let { "?$REDIRECT_URL_PARAM_NAME=${URLEncoder.encode(it, Charsets.UTF_8)}" } ?: ""
-                response.headers.add(HttpHeaders.LOCATION, "/$LOGOUT_CUSTOM_ENDPOINT$redirectUrl")
-                Mono.empty()
-            }
-    }
-
-    companion object {
-        const val REDIRECT_URL_PARAM_NAME = "redirect_url"
-        const val LOGOUT_CUSTOM_ENDPOINT = "logout-form"
     }
 }
