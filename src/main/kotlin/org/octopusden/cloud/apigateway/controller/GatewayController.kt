823d33d31245cb13e4ce513d463926c2074ba9cf
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2025-12-08 16:30:36 +0300

    Filter routes

--- src/main/kotlin/org/octopusden/cloud/apigateway/controller/GatewayController.kt
+++ src/main/kotlin/org/octopusden/cloud/apigateway/controller/GatewayController.kt
@@ -8,11 +8,17 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class GatewayController(private val gatewayProperties: GatewayProperties) {
+class GatewayController(gatewayProperties: GatewayProperties) {
+    private val routeIds = gatewayProperties.routes.filter { route ->
+        route.predicates.any { predicate ->
+            predicate.name == "Path" && predicate.args.values.any { it == "/${route.id}/**" }
+        }
+    }.map { it.id }
+
     @GetMapping
     fun index(model: Model, @AuthenticationPrincipal oauth2User: OAuth2User): String {
         model.addAttribute("userName", oauth2User.name)
-        model.addAttribute("routeIds", gatewayProperties.routes.map { it.id })
+        model.addAttribute("routeIds", routeIds)
         return "index"
     }
 }
