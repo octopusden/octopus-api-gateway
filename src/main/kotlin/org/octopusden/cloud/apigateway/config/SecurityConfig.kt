280243ffa8e8c38a1c1a6d1a8985a40d44ac92bb
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2025-12-08 17:45:56 +0300

    Use back-channel logout

--- src/main/kotlin/org/octopusden/cloud/apigateway/config/SecurityConfig.kt
+++ src/main/kotlin/org/octopusden/cloud/apigateway/config/SecurityConfig.kt
@@ -1,26 +1,19 @@
 package org.octopusden.cloud.apigateway.config
 
 import org.octopusden.cloud.commons.security.client.AuthServerClient
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
 import org.springframework.security.web.server.SecurityWebFilterChain
-import reactor.core.publisher.Mono
 
 @Configuration
 @EnableWebFluxSecurity
 @Import(AuthServerClient::class)
-open class SecurityConfig(
-    @Value("\${auth-server.logout-url}")
-    private val logoutUrl: String
-) {
+open class SecurityConfig {
     @Bean
     open fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         http.authorizeExchange { exchanges: AuthorizeExchangeSpec ->
@@ -28,13 +21,7 @@ open class SecurityConfig(
             exchanges.anyExchange().permitAll()
         }
             .oauth2Login(Customizer.withDefaults())
-            .logout { logout ->
-                logout.logoutSuccessHandler { exchange, _ ->
-                    exchange.exchange.response.statusCode = HttpStatus.FOUND
-                    exchange.exchange.response.headers.add(HttpHeaders.LOCATION, logoutUrl)
-                    Mono.empty()
-                }
-            }
+            .oidcLogout { it.backChannel(Customizer.withDefaults()) }
             .csrf { it.disable() }
         return http.build()
     }
